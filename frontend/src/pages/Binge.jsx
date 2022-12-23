bd125066cc6522136cb702dde7bfc064262ea6f8
Hammad <hammadsultan07@gmail.com>
2022-12-23 21:28:42 +0530

    first

--- frontend/src/pages/Binge.jsx
+++ frontend/src/pages/Binge.jsx
@@ -1,44 +1,28 @@
 import React from 'react'
 import Cards from '../components/Cards';
 import data from '../backend/Data';
+import { useState } from 'react';
 const Binge = () => {
+  const[foodState,setFoodState]=useState("");
+
   return (
-    <div>
-      <form>
-  <div className="form-group">
-    <label for="exampleFormControlSelect1">Select the series</label>
-    <select className="form-control" id="FormControlSelect1">
-      <option value="1">Breaking Bad</option>
-      <option value="2">Breaking Bad</option>
-      <option value="3">Breaking Bad</option>
-      <option value="4">Breaking Bad</option>
-      <option value="5">Breaking Bad</option>
-      <option value="6">Breaking Bad</option>
-      <option value="7">Breaking Bad</option>
-      <option value="8">Breaking Bad</option>
-    </select>
-  </div>
-  <button type="submit" className="btn btn-primary" onSubmit={()=>Recommend()}>Submit</button>
-</form>
+    <div className='container pt-5'>
+      <select className='custom-select' onChange={(e)=>{
+        const selectedSeries=e.target.value;
+        setFoodState(selectedSeries)
+      }}>
+        <option value="1">Breaking bad</option>
+        <option value="2">Stranger things</option>
+        <option value="3">ALice</option>
+        <option value="4">Kissing booth</option>
+        <option value="5">Euphoria</option>
+        <option value="6">Brea</option>
+        <option value="7">Bread</option>
+        <option value="8">Brng bad</option>
+      </select>
+      <br />
+      {foodState}
     </div>
   )
 }
-
-function Recommend()
-{
-    return(<div className='container-fluid'>
-    <h3 className='text-center mt-5 text-uppercase'>Shop Page</h3>
-    <div className="container py-4">
-        <div className="row">
-            {data.products.map((item, index) => {
-                return (
-                    <Cards img={item.img} title={item.title} desc={item.desc} price={item.price} item={item} key={index}/>
-                )
-            })}
-            
-        </div>
-    </div>
-</div>);
-}
-
 export default Binge
